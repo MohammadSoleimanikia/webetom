77ee66897e64aca9e72c3b74e6c48748970d320a
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2026-09-06 10:44:24 +0330

    data: add description to feature items

--- src/const/FEATURES.ts
+++ src/const/FEATURES.ts
@@ -16,25 +16,30 @@ export const FEATURES: FeatureItem[] = [
     title: "تحویل حضوری",
     icon: PiHandshake,
     color: "text-custom-blue",
+    description:"سایت و مدارک را حضوری تحویل می دهیم"
   },
   {
     title: "آموزش حضوری",
     icon: FaChalkboardTeacher,
-    color: "text-custom-green",
+    color: "text-custom-purple",
+    description:"آموزش کامل کار با سایت به صورت حضوری"
   },
   {
     title: "قرارداد حضوری",
     icon: FaFileSignature,
-    color: "text-custom-purple",
+    color: "text-secondary",
+    description:'قرارداد  رسمی و شفاف به صورت حضوری '
   },
   {
     title: "پشتیبانی حضوری",
     icon: MdSupportAgent,
     color: "text-custom-green",
+    description:' پشتیبانی سریع و حضوری هر زمان که نیاز داشته باشید'
   },
   {
     title: "نصب در کمتر از 24 ساعت",
     icon: VscRocket,
     color: "text-custom-blue",
+    description:'سایت شما در کمتر از 24 ساعت آماده تحویل است'
   },
 ];
