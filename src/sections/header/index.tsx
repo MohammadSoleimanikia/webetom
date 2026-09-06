cf1dc90ddf33b9f2975d3a9e57caead57c47c7b0
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2026-09-06 10:45:51 +0330

    refactor (header): add container

--- src/sections/header/index.tsx
+++ src/sections/header/index.tsx
@@ -10,51 +10,45 @@ export default function Header() {
 
     <>
       <header className="relative z-20 hidden h-20 w-full bg-white lg:block 2xl:h-28">
-        <div className="mx-auto flex h-full w-full items-center">
-          {/* Logo Section */}
-          <div className="relative z-30 h-full w-4/12">
-            {/*  Shape */}
-            <div
-              className={clsx(
-                "absolute top-0 left-0 z-0",
-                "h-[calc(100%+30px)] w-full",
-                "rounded-bl-[100px] bg-white xl:rounded-bl-full",
-              )}
-            />
+        {/* Logo shape */}
+        <div className="absolute top-0 right-0 z-30 h-full w-[33.333%]">
+          <div
+            className={clsx(
+              "absolute top-0 right-0",
+              "h-[calc(100%+30px)] w-full",
+              "rounded-bl-[100px] bg-white xl:rounded-bl-full",
+            )}
+          />
 
-            {/* Logo Content */}
-            <div
-              className={clsx(
-                "absolute top-0 left-0 z-10",
-                "flex h-[calc(100%+30px)] w-full",
-                "flex-col items-center justify-center",
-                "pl-10",
-              )}
-            >
-              {/* Logo */}
-              <div className="flex flex-col items-center justify-center">
-                <Image
-                  preload
-                  width={0}
-                  height={0}
-                  src="/images/logo.svg"
-                  alt="Logo"
-                  className="h-10 w-auto object-contain xl:h-14"
-                />
+          {/* Logo content */}
+          <div className="relative z-10 flex h-[calc(100%+30px)] w-full flex-col items-center justify-center">
+            <div className="flex flex-col items-center justify-center">
+              <Image
+                preload
+                width={0}
+                height={0}
+                src="/images/logo.svg"
+                alt="Logo"
+                className="h-10 w-auto object-contain xl:h-14"
+              />
 
-                <Typography variant="h2" className="mt-2 text-base xl:text-xl">
-                  سایت فروشگاهی آماده
-                </Typography>
-              </div>
+              <Typography variant="h2" className="mt-2 text-base xl:text-xl">
+                سایت فروشگاهی آماده
+              </Typography>
             </div>
           </div>
-
-          {/* Navigation */}
-          <div className="flex h-full w-9/12 items-center justify-between pl-10">
-            <NavDesktop />
-            <OrderButton className="hidden xl:flex py-2.5" />
-          </div>
         </div>
+
+        {/* left side*/}
+        <Container
+          maxWidth="xxl"
+          className="relative  z-20  flex h-full items-center justify-between"
+        >
+          <div className="mr-[33.333%] flex h-full w-full items-center justify-between ">
+            <NavDesktop />
+            <OrderButton className="hidden py-2.5 xl:flex" />
+          </div>
+        </Container>
       </header>
 
       {/* mobile------------------------------------------------------------- */}
