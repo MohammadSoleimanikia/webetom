7ebf5019c63d7a947bd100cc80a9800332ce7250
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2026-09-07 10:51:30 +0330

    theme: add extra small shadow

--- src/theme/shadow.ts
+++ src/theme/shadow.ts
@@ -4,5 +4,6 @@ export const shadows = [
   "none",
   "0px 0px 20px 1px #00000024",
   "0px 0px 10px 1px #00000024",
-  ...Array(22).fill("none"),
+  "0px 0px 3px 1px #00000024",
+  ...Array(21).fill("none"),
 ] as Shadows;
