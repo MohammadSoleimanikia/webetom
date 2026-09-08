a81ac671fe338bb3a26a384f1772d5ce50b0ec29
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2026-09-08 16:25:23 +0330

    style: change icons

--- src/const/WHYUS.tsx
+++ src/const/WHYUS.tsx
@@ -1,49 +1,57 @@
 import { FeatureItem } from "./FEATURES";
 import { TbShieldCheck } from "react-icons/tb";
-import { VscRocket } from "react-icons/vsc";
-import { MdOutlineDiscount } from "react-icons/md";
+import { IoRocketOutline } from "react-icons/io5";
+import { IoPricetagsOutline } from "react-icons/io5";
 import { HiOutlineUserGroup } from "react-icons/hi2";
-import { IoDiamondOutline } from "react-icons/io5";
-import { MdSupportAgent } from "react-icons/md";
+import { IoColorPaletteOutline } from "react-icons/io5";
+import { TbHeadset } from "react-icons/tb";
 import { TbSeo } from "react-icons/tb";
 export const WHY_US:FeatureItem[]=[
   {
     title: "طراحی حرفه ای",
-    icon: IoDiamondOutline,
+    icon: IoColorPaletteOutline,
     description: "قالب های اختصاصی و واکنش گرا",
+    color:'text-custom-blue'
   },
   {
     title:"امنیت و پایداری" ,
     icon:TbShieldCheck ,
     description:"استفاده از سرورهای پر سرعت و گواهینامه SSL" ,
+    color:'text-custom-purple'
   },
   {
     title:"قیمت شفاف" ,
-    icon: MdOutlineDiscount,
+    icon: IoPricetagsOutline,
     description: "هزینه مشخص و بدون هزینه پنهان",
+    color:'text-custom-green'
   },
   {
     title:"سرعت بالا " ,
-    icon:VscRocket ,
+    icon:IoRocketOutline ,
     description: (
       <>
         نصب و راه‌اندازی در کمتر از <span className="text-secondary">24</span> ساعت
       </>
     ),
+    color:'text-custom-red'
   },
+  
   {
     title: "پشتیبانی واقعی",
-    icon:MdSupportAgent ,
+    icon:TbHeadset ,
     description: "تیم پشتیبانی همیشه در کنار شما",
+    color:'text-primary'
   },
   {
     title: "سئوی قوی",
     icon: TbSeo,
     description: "ساختار سئو شده برای گوگل",
+    color:'text-secondary'
   },
   {
     title:"حضور واقعی" ,
     icon:HiOutlineUserGroup ,
     description: "همراه شما در مشهد از آموزش تا پشتیبانی",
+    color:'text-custom-green'
   },
 ]
