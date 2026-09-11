cb40ab158c1f7928427070c42790cad92cbf4f27
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2026-09-11 12:14:31 +0330

    UI: add light bg to text in mobile for better view, make text section wider

--- src/sections/hero/index.tsx
+++ src/sections/hero/index.tsx
@@ -6,16 +6,15 @@ import HeroFeatures from "./heroFeatures";
 export default function HeroSection() {
   return (
     <section className="relative">
-      
       {/* character image */}
       <div className="flex min-h-64 sm:min-h-64 md:min-h-80">
-        <div className="relative w-1/2">
+        <div className="relative w-5/12 sm:w-1/2">
           <Image
             preload
             src="/images/hero/character.webp"
             alt="character in hero section"
             fill
-            className="mask-[linear-gradient(to_bottom,black_90%,transparent_100%)] hidden object-contain object-bottom-right sm:block"
+            className="hidden mask-[linear-gradient(to_bottom,black_90%,transparent_100%)] object-contain object-bottom-right sm:block"
           />
           <Image
             preload
@@ -28,9 +27,14 @@ export default function HeroSection() {
         </div>
 
         {/* text section */}
-        <div className="flex w-1/2 flex-col items-center justify-center space-y-10 md:space-y-15">
+        <div className="flex w-7/12 sm:w-1/2 flex-col items-center justify-center space-y-10 md:space-y-15">
           {/* texts */}
-          <div className=" text-center flex w-full flex-col items-center justify-center">
+          <div
+            className={clsx(
+              "bg-primary-lighter/50 md:bg-transparent flex w-full flex-col items-center",
+              "justify-center rounded-2xl text-center py-2",
+            )}
+          >
             <Typography
               variant="h2"
               className={clsx(
@@ -54,11 +58,12 @@ export default function HeroSection() {
             <Typography
               variant="body1"
               className={clsx(
-                "text-base block mt-2 md:mt-5 font-semibold sm:text-lg md:text-2xl lg:text-3xl",
+                " block text-base font-medium sm:text-lg md:text-2xl lg:text-3xl",
+                "mt-4 md:mt-5"
               )}
             >
-              --نصب و راه اندازی در{" "}
-              کمتر از<span className="text-secondary"> 24 </span>  ساعت--
+              --نصب و راه اندازی در کمتر از
+              <span className="text-secondary"> 24 </span> ساعت--
             </Typography>
           </div>
           {/* CTA buttons */}
@@ -69,7 +74,6 @@ export default function HeroSection() {
 
       {/* CTA */}
       <CtaButtons className="absolute bottom-2 mt-1 flex sm:hidden" />
-
     </section>
   );
 }
