a4248cec39899370dcee30dfa5c3f760e432382e
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2026-09-12 13:29:30 +0330

    UI: redesign user comment

--- src/sections/voiceOfCustomers/VocCard.tsx
+++ src/sections/voiceOfCustomers/VocCard.tsx
@@ -10,11 +10,32 @@ type Props = {
 
 export default function VocCard({ item }: Props) {
   return (
-    <div className="shadow-card-small bg-background flex min-h-60 w-full flex-col rounded-xl p-5">
+    <div className="shadow-card-small  bg-white flex justify-between min-h-60 w-full flex-col rounded-xl p-5">
+      {/* rating stars */}
+      <div className="flex items-center gap-1">
+        {Array.from({ length: 5 }, (_, index) => (
+          <AiFillStar
+            key={index}
+            className={clsx(
+              "size-4",
+              index < item.rating ? "text-yellow-400" : "text-gray-300",
+            )}
+          />
+        ))}
+      </div>
+
+      {/* comment of the user */}
+      <Typography
+        variant="body2"
+        className="py-3 text-base font-medium text-gray-500"
+      >
+        {item.comment}
+      </Typography>
+
       {/* customer detail */}
-      <div className="flex items-center gap-3">
+      <div className="flex items-center gap-3 border-t border-t-gray-200 pt-3 ">
         {/* image container */}
-        <div className="relative size-20 overflow-hidden rounded-full">
+        <div className="relative size-14 overflow-hidden rounded-full">
           <Image
             src={item.avatarSrc}
             alt="costumer pic"
@@ -24,37 +45,17 @@ export default function VocCard({ item }: Props) {
         </div>
 
         {/* customer detail text */}
-        <div>
-          <Typography variant="body2" className="text-base font-semibold">
+        <div >
+          <Typography variant="body2" className="text-sm font-medium">
             {item.name}
           </Typography>
           <Typography
             variant="body1"
-            className="text-sm font-medium text-gray-500"
+            className="text-xs font-medium text-gray-500"
           >
             {item.profession}
           </Typography>
         </div>
-      </div>
-      {/* comment of the user */}
-      <Typography
-        variant="body2"
-        className="py-3 text-base font-medium text-gray-500"
-      >
-        {item.comment}
-      </Typography>
-
-      {/* rating stars */}
-      <div className="mt-auto flex items-center gap-1">
-        {Array.from({ length: 5 }, (_, index) => (
-          <AiFillStar
-            key={index}
-            className={clsx(
-              "size-6",
-              index < item.rating ? "text-yellow-400" : "text-gray-300",
-            )}
-          />
-        ))}
       </div>
     </div>
   );
