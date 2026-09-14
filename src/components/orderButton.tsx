206f24339e8b4e5572a1da58b5a70cb3b4b30d0e
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2026-09-14 11:05:03 +0330

    refactor: add link to btn

--- src/components/orderButton.tsx
+++ src/components/orderButton.tsx
@@ -1,31 +1,36 @@
-import { Button, Typography, type ButtonProps } from "@mui/material";
-import { IoCartOutline } from "react-icons/io5";
+import { routes } from "@/const/links";
+import { Button, type ButtonProps } from "@mui/material";
 import clsx from "clsx";
+import Link from "next/link";
+import { TbShoppingCart } from "react-icons/tb";
 
 type Props = {
   variant?: "primary" | "secondary" | "icon";
+  text?:string
 } & Omit<ButtonProps, "variant">;
 
 export default function OrderButton({
   className = "",
   variant = "primary",
+  text="سفارش سایت",
   ...props
 }: Props) {
   const isIcon = variant === "icon";
-
   return (
+    
     <Button
+      LinkComponent={Link}
+      href={routes.order}
       {...props}
       variant={isIcon ? "text" : "contained"}
       endIcon={
         !isIcon && (
-          <IoCartOutline className={clsx(isIcon && "size-7 sm:size-9")} />
+          <TbShoppingCart  className={clsx(isIcon && "size-7 sm:size-9 ")} />
         )
       }
       className={clsx(
         "h-auto",
         variant === "secondary" && "bg-secondary",
-
         // Icon variant
         isIcon && "text-primary m-0 size-auto min-w-0 p-0",
 
@@ -36,10 +41,10 @@ export default function OrderButton({
       )}
     >
       {isIcon && (
-        <IoCartOutline className={clsx(isIcon && "size-7 sm:size-9")} />
+        <TbShoppingCart className={clsx(isIcon && "size-7 stroke-2 text-gray-600 sm:size-9")} />
       )}
 
-      {!isIcon && " سفارش سایت"}
+      {!isIcon && text}
     </Button>
   );
 }
