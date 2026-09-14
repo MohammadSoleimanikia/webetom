691824aeb07cb66dceed0ed7f15bcdaf6da33b6d
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2026-09-14 11:00:41 +0330

    refactor: add links to items

--- src/layout/footer/socialSection/socialSection.tsx
+++ src/layout/footer/socialSection/socialSection.tsx
@@ -1,28 +1,32 @@
-import { Typography } from "@mui/material";
+import {  Typography } from "@mui/material";
 import Image from "next/image";
 
 import { IoLogoInstagram, IoLogoWhatsapp } from "react-icons/io5";
 import { LiaTelegram } from "react-icons/lia";
 import SocialIconWrapper from "./socialIconWrapper";
+import Link from "next/link";
+import { routes } from "@/const/links";
 
 export default function SocialSection() {
   return (
     <div className="flex w-full flex-col items-center space-y-3 text-center">
       {/* logo + title */}
-      <div className="flex w-full flex-col items-center gap-1">
-        <div className="relative h-10 w-1/2">
-          <Image
-            fill
-            alt="logo of sitom"
-            className="object-contain"
-            src="/images/logo-light.svg"
-          />
-        </div>
+      <Link className="w-full" href={routes.home}>
+        <div className="flex w-full flex-col items-center gap-1">
+          <div className="relative h-10 w-1/2">
+            <Image
+              fill
+              alt="logo of sitom"
+              className="object-contain"
+              src="/images/logo-light.svg"
+            />
+          </div>
 
-        <Typography variant="h2" className="text-sm">
-          سایت فروشگاهی آماده
-        </Typography>
-      </div>
+          <Typography variant="h2" className="text-sm">
+            سایت فروشگاهی آماده
+          </Typography>
+        </div>
+      </Link>
 
       {/* site description */}
       <Typography variant="body1" className="max-w-md py-3">
@@ -31,7 +35,7 @@ export default function SocialSection() {
       </Typography>
 
       {/* icons */}
-      <div className="flex w-full justify-between">
+      <div className="flex w-full justify-center gap-5">
         <SocialIconWrapper link="https://www.instagram.com/webeto.co">
           <IoLogoWhatsapp className="size-6" />
         </SocialIconWrapper>
